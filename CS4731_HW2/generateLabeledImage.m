0e6e4ce2e0082d54e259305cfd3ab61e090a51ec
Emily Schultz <emily.schultz@newscred.com>
2014-02-26 22:04:26 -0500

    A2:C1a fixed and finished

--- CS4731_HW2/generateLabeledImage.m
+++ CS4731_HW2/generateLabeledImage.m
@@ -1,5 +1,6 @@
 function labeled_img = generateLabeledImage(gray_img, threshold)
-bw_img = im2bw(gray_img, threshold);
+% only pixels larger than threshold value
+bw_img = (gray_img > threshold * 255);
 
 [labels, n] = bwlabel(bw_img);
 labeled_img = labels;
