06510ee5e57c40dd81e833becb568d5b59610751
Emily Schultz <emily.schultz@newscred.com>
2014-04-15 03:07:57 -0400

    HW4:c1c completed

--- CS4731_HW4/computeMask.m
+++ CS4731_HW4/computeMask.m
@@ -1,1 +1,12 @@
 function mask = computeMask(img_cell)
+mask = zeros(size(img_cell{1}));
+
+for r=1:size(mask,1)
+    for c=1:size(mask,2)
+        for i=1:size(img_cell)
+            if img_cell{i}(r,c) ~= 0
+                mask(r,c) = 1;
+            end;
+        end;
+    end;
+end;
